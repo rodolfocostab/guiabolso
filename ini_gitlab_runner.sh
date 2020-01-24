2f74ae9b4436c71a2642a7bf23cdfc14193d05ef
Rodolfo Barbosa <rodolfo.barbosa@arizona.global>
2020-01-24 15:27:06 -0300

    Criado shell para runner com as intrucoes do CICD

---
+++ ini_gitlab_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instalação do gitlab-runner
+
+sudo apt install -y gitlab-runner
+
+# Iniciar gitlab-runner
+
+sudo systemctl start gitlab-runner
+
+# Registro no projeto deploy-api o shell para CI/CD
+
+sudo gitlab-runner register -n --url "https://gitlab.com/" --registration-token "ssMxGqCS-Py2yQNwQWvT" --executor "shell" --tag-list "shell" --description "local-shell" --locked="false"
